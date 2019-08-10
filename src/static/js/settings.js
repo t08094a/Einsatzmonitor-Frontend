9acfdc98f2109d5e94d83936d6a0701f6a70af7b
Finn Winter <22560479+Finn0811@users.noreply.github.com>
2019-08-10 16:21:20 +0200

    :art: Add config menu titles

--- src/static/js/settings.js
+++ src/static/js/settings.js
@@ -147,3 +147,20 @@ let settingsModel = new SettingsModel();
 settingsModel.loadSettings();
 
 ko.applyBindings(settingsModel);
+
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "3000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
