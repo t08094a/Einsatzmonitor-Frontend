f90877b73d3765f515d756ef22043f9e0f56b370
Finn Winter <22560479+Finn0811@users.noreply.github.com>
2021-10-21 19:58:44 +0200

    :bug: Raise max log file size

--- src/common/common.ts
+++ src/common/common.ts
@@ -53,6 +53,7 @@ export const em = new emitter();
 
 export const logger = require('electron-log')
 logger.transports.file.sync = false;
+logger.transports.file.maxSize = 104857600;
 
 export const alamosFeedbackUrl = (dbId: string) => `https://apager-firemergency-2.appspot.com/fe2/feedback?dbId=${dbId}`;
 
